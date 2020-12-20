4896d14793c289a9b46430339c21f0d7c32c4699
tathran <dustin.lahr.367@gmail.com>
2020-12-19 23:24:41 -0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # spaceduck-emacs
-A quick and dirty space duck theme for emacs
+A quick and dirty spaceduck theme for emacs generated with <https://github.com/UwUnyaa/vimco>. Lots of hackery at this stage. See below.
+
+## Modline modifications
+
+add the following to your `init.el` or `emacs.el`
+
+```lisp
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(mode-line-buffer-id ((t (:foreground "#5ccc96" nil)))))
+
+```
+
+```lisp
+(defface mode-line-directory
+  '((t :background "black" :foreground "#7a5ccc" :bold t))
+  "Face used for buffer identification parts of the mode line."
+  :group 'mode-line-faces
+  :group 'basic-faces)
+```
+
+```lisp
+(set-face-attribute 'mode-line nil
+                    :background "black"
+                    :foreground "#686f9a"
+                    ; :box '(:line-width 1 :color "black")
+                    :overline nil
+                    :underline nil)
+
+(set-face-attribute 'mode-line-inactive nil
+                    :background "#2e3459"
+                    :foreground "#2e3459"
+                    ; :box '(:line-width 1 :color "black")
+                    :overline nil)
+```
+                    
+# Screenshot
+
+![emacs](emacs.png)
---
+++ spaceduck-theme.el
@@ -0,0 +1,133 @@
+;;; spaceduck-theme.el --- Custom face theme for Emacs
+
+;; This theme was generated with vimco.el
+;; You can get it from:
+;; <https://github.com/UwUnyaa/vimco>
+
+;;; Code:
+
+(deftheme spaceduck)
+
+(custom-theme-set-faces 'spaceduck
+                        '(font-lock-keyword-face
+                          ((((class color)
+                             (min-colors 89))
+                            (:foreground "#e39400"))))
+                        '(font-lock-function-name-face
+                          ((((class color)
+                             (min-colors 89))
+                            (:foreground "#5ccc96"))))
+                        '(font-lock-string-face
+                          ((((class color)
+                             (min-colors 89))
+                            (:foreground "#00a3cc"))))
+                        '(font-lock-warning-face
+                          ((((class color)
+                             (min-colors 89))
+                            (:foreground "#e33400"))))
+                        '(underline
+                          ((((class color)
+                             (min-colors 89))
+                            (:underline t :foreground "#5ccc96"))))
+                        '(font-lock-type-face
+                          ((((class color)
+                             (min-colors 89))
+                            (:foreground "#ce6f8f"))))
+                        '(font-lock-preprocessor-face
+                          ((((class color)
+                             (min-colors 89))
+                            (:foreground "#b3a1e6"))))
+                        '(font-lock-builtin-face
+                          ((((class color)
+                             (min-colors 89))
+                            (:foreground "#5ccc96"))))
+                        '(font-lock-variable-name-face
+                          ((((class color)
+                             (min-colors 89))
+                            (:foreground "#00a3cc"))))
+                        '(font-lock-constant-face
+                          ((((class color)
+                             (min-colors 89))
+                            (:foreground "#f2ce00"))))
+                        '(font-lock-doc-face
+                          ((((class color)
+                             (min-colors 89))
+                            (:foreground "#2e3459"))))
+                        '(font-lock-comment-face
+                          ((((class color)
+                             (min-colors 89))
+                            (:foreground "#2e3459"))))
+                        '(cursor
+                          ((((class color)
+                             (min-colors 89))
+                            (:background "#818596" :foreground "#0f111b"))))
+                        '(show-paren-match-face
+                          ((((class color)
+                             (min-colors 89))
+                            (:inverse-video t :background "#2e3459" :foreground "#ffffff"))))
+                        '(default
+                           ((((class color)
+                              (min-colors 89))
+                             (:background "#0f111b" :foreground "#ecf0c1"))))
+                        '(highline-face
+                          ((((class color)
+                             (min-colors 89))
+                            (:underline t :background "#16172d"))))
+                        '(ac-selection-face
+                          ((((class color)
+                             (min-colors 89))
+                            (:background "#272c42" :foreground "#f2ce00"))))
+                        '(ac-candidate-face
+                          ((((class color)
+                             (min-colors 89))
+                            (:background "#2e3459" :foreground "#ecf0c1"))))
+                        '(flyspell-duplicate
+                          ((((class color)
+                             (min-colors 89))
+                            (:underline t :foreground "#f2ce00"))))
+                        '(flyspell-incorrect
+                          ((((class color)
+                             (min-colors 89))
+                            (:underline t :foreground "#e33400"))))
+                        '(region
+                          ((((class color)
+                             (min-colors 89))
+                            (:inverse-video t :background "#272c42"))))
+                        '(mode-line-inactive
+                          ((((class color)
+                             (min-colors 89))
+                            (:inverse-video t))))
+                        '(mode-line-buffer-id
+                          ((((class color)
+                             (min-colors 89))
+                            (:inverse-video t :background "#0f111b" :foreground "#2e3459"))))
+                        '(mode-line
+                          ((((class color)
+                             (min-colors 89))
+                            (:inverse-video t :background "#0f111b" :foreground "#ecf0c1"))))
+                        '(fringe
+                          ((((class color)
+                             (min-colors 89))
+                            (:underline t :background "#0f111b" :foreground "#2e3459"))))
+                        '(linum
+                          ((((class color)
+                             (min-colors 89))
+                            (:underline t :background "#0f111b" :foreground "#2e3459"))))
+                        '(isearch
+                          ((((class color)
+                             (min-colors 89))
+                            (:inverse-video t :background "#114fd6" :foreground "#ffffff"))))
+                        '(isearch-lazy-highlight-face
+                          ((((class color)
+                             (min-colors 89))
+                            (:inverse-video t :background "#272c42" :foreground "#ffffff"))))
+                        '(dired-directory
+                          ((((class color)
+                             (min-colors 89))
+                            (:weight bold :foreground "#00a3cc")))))
+
+(provide-theme 'spaceduck)
+
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
